4197aadfaae7c4c33ee9cc65e238da5cab50c7c4
mts0629 <soratobi93@gmail.com>
2025-08-27 23:59:17 +0900

    Add header comments

--- simple/src/my_allocator.h
+++ simple/src/my_allocator.h
@@ -1,7 +1,10 @@
 #include <stddef.h>
 
+// Total size of memory block
 #define TOTAL_SIZE 200
 
+// Allocate a specified size memory
 void *my_malloc(const size_t size);
 
+// Free specified memory
 void my_free(void *ptr);
